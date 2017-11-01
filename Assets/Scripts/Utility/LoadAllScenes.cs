09835cc39c3f542da27ae14f9f8452e059f495c4
Emil <emimvi@gmail.com>
2017-11-01 18:22:00 +0100

    Utility script to load all scenes

---
+++ Assets/Scripts/Utility/LoadAllScenes.cs
@@ -0,0 +1,24 @@
+﻿//Author: Emil Villumsen
+//Collaborator: Jonathan,
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Events;
+
+public class LoadAllScenes : Singleton<LoadAllScenes>
+{
+
+    void Start()
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            Scene scene = SceneManager.GetSceneByBuildIndex(i);
+            if (!scene.isLoaded)
+            {
+                SceneManager.LoadScene(i, LoadSceneMode.Additive);
+            }
+        }
+    }
+}
