ea7abe861364cb44c3af6dd300b076957a5abc7b
Emil <emimvi@gmail.com>
2017-11-01 17:32:23 +0100

    Add manager instantiation

---
+++ Assets/Scripts/Systems/ManagerInstantiator.cs
@@ -0,0 +1,18 @@
+﻿//Author: Emil Villumsen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerInstantiator : MonoBehaviour
+{
+    public GameObject managerPrefab;
+
+    void Awake()
+    {
+        if (GameObject.FindWithTag("ManagerParent") == null)
+        {
+            Instantiate(managerPrefab);
+            //Instantiate(managerPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿//Author: Emil Villumsen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+}
