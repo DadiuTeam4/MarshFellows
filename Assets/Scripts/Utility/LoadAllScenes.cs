80017bbb87838b1f24a42aed4980746a5578eb52
Emil <emimvi@gmail.com>
2017-11-02 17:06:47 +0100

    First playable protottype

--- Assets/Scripts/Utility/LoadAllScenes.cs
+++ Assets/Scripts/Utility/LoadAllScenes.cs
@@ -3,7 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+
 using UnityEngine.SceneManagement;
 
 using Events;
@@ -13,12 +13,12 @@ public class LoadAllScenes : Singleton<LoadAllScenes>
 {
     void Start()
     {
-        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
         {
-			if (i == 1) // HARDCODED SO TITLESCREEN ISNT LOADED WITH THIS, THIS IS ONLY TO GET FPP IN TIME!!!
+			/*if (i == 0) // HARDCODED SO TITLESCREEN ISNT LOADED WITH THIS, THIS IS ONLY TO GET FPP IN TIME!!!
 			{
 				continue;
-			}
+			}*/
             Scene scene = SceneManager.GetSceneByBuildIndex(i);
             if (!scene.isLoaded)
             {
@@ -28,7 +28,7 @@ public class LoadAllScenes : Singleton<LoadAllScenes>
                 }
                 else
                 {
-                    SceneManager.LoadScene(EditorBuildSettings.scenes[i].path, LoadSceneMode.Additive);
+                //    SceneManager.LoadScene(EditorBuildSettings.scenes[i].path, LoadSceneMode.Additive);
                 }
             }
         }
