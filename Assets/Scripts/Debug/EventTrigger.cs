6afc1dbfae6898e9d1e769a83d61d41b41f07ac5
Mathias Dam Hedelund <mdhe@itu.dk>
2017-11-02 09:54:27 +0100

    Event triggering tests

---
+++ Assets/Scripts/Debug/EventTrigger.cs
@@ -0,0 +1,18 @@
+﻿// Author: Mathias Dam Hedelund
+// Contributors: 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour 
+{
+	void OnEnable()
+	{
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
