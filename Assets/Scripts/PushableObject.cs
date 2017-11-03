38259aba7d62128e57fcbacf743467f604bfbebf
IYavin <itai.yavin@gmail.com>
2017-11-03 08:46:50 +0100

    WIP push objects on swipe

---
+++ Assets/Scripts/PushableObject.cs
@@ -0,0 +1,24 @@
+﻿// Author: Itai Yavin
+// Contributors: 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushableObject : Swipeable 
+{
+	Rigidbody rigidbody;
+	public float forceScalar = 2.0f;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();	
+	}
+
+	public override void OnSwipe(RaycastHit raycastHit, Vector3 direction) 
+	{
+		Vector3 force = new Vector3(direction.x, 0, direction.y);
+		rigidbody.AddForce(force * forceScalar);
+	}
+}
