53bebb0c3cda9a4e04f91e5da3e8f17f603d2141
Unknown <tilemachospantazis8@gmail.com>
2017-11-07 10:56:02 +0100

    Working load save with example

---
+++ Assets/TestSaveScript.cs
@@ -0,0 +1,31 @@
+﻿//Author:Tilemachos
+//Co-author:
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSaveScript : MonoBehaviour {
+GameStateManager newRound = new GameStateManager();
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("n"))
+		{
+			
+			newRound.playedBefore = true;
+			newRound.roundsPlayed++;
+			GameStateManager.current = newRound;
+			print("N Played before = "+newRound.roundsPlayed);
+			
+		}
+		if (Input.GetKeyDown("s"))
+		{
+			SaveLoadManager.Save();
+		}
+
+		if (Input.GetKeyDown("l"))
+		{
+			SaveLoadManager.Load();
+			print("L Played before = "+GameStateManager.current.roundsPlayed);
+		}
+	}
+}
