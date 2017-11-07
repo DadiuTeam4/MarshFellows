637e4e9b4eadd04092c6aa3ce2d37be5caed8b40
Unknown <tilemachospantazis8@gmail.com>
2017-11-07 16:07:23 +0100

    Generating obstacle to block path

--- Assets/Entities/ChangeSceneTrigger/ChangeSceneEmitter.cs
+++ Assets/Entities/ChangeSceneTrigger/ChangeSceneEmitter.cs
@@ -8,10 +8,14 @@ using Events;
 //0 for sending current scene , 1 for sending next in order to load, 2 for sending another one
 public class ChangeSceneEmitter : MonoBehaviour {
 
+    public GameObject blocker;
+    public int offsetForCreatingObstacle = 10;
     public string currentScene = "";
     public string basicSceneToLoad="";
     public string secondarySceneToLoad="";
     private string emptyString = "";
+    Collider m_ObjectCollider;
+
     void OnTriggerEnter(Collider other)
     {
         EventManager eventManager = EventManager.GetInstance(); 
@@ -38,7 +42,11 @@ public class ChangeSceneEmitter : MonoBehaviour {
             argument.intComponent = 2;           
             eventManager.CallEvent(CustomEvent.LoadScene,argument);
         }
+
+        Instantiate(blocker, transform.position + (transform.forward*-offsetForCreatingObstacle), this.gameObject.transform.rotation);
         Destroy(this.gameObject);
+
+
     }
 
 }
