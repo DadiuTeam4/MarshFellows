9c4c5f8a392b810d55c8cd45ddc3d66a218fb5ed
Unknown <tilemachospantazis8@gmail.com>
2017-11-09 15:13:56 +0100

    Huge collider that stays undergrown that freezes objects upon collision

---
+++ Assets/Entities/StaySinked/KeepObjectUnder.cs
@@ -0,0 +1,17 @@
+﻿//Author:Tilemachos
+//Co-author:
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepObjectUnder : MonoBehaviour {
+
+private Collider parentCollider;
+private Collider myCollider;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+		rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+	}
+}
