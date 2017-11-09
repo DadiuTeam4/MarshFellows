9b17f66f6353cc9edd873446cc938686d453e628
IYavin <itai.yavin@gmail.com>
2017-11-09 09:55:33 +0100

    WIP

---
+++ Assets/Scripts/WindGust.cs
@@ -0,0 +1,25 @@
+﻿// Author: Itai Yavin
+// Contributors: 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Events;
+public class WindGust : MonoBehaviour 
+{
+	private WindZone windZone;
+	private ParticleSystem windParticles;
+
+	private EventDelegate eventDelegate;
+
+	void Start () 
+	{	
+		eventDelegate = PlaceGust;
+	}
+
+	public void PlaceGust(EventArgument argument)
+	{
+		transform.position = argument.raycastComponent.point;
+	}
+}
