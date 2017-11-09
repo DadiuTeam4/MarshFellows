dc8abf44dd486fca5694be9a9735910e6688d837
Jonathan <madsof4@hotmail.com>
2017-11-09 10:08:48 +0100

    Foreshadow triggerzone and event prefab added

---
+++ Assets/Scripts/Foreshadow/ForeshadowTriggerZone.cs
@@ -0,0 +1,20 @@
+﻿// Author: Jonathan
+// Contributers:
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeshadowTriggerZone : MonoBehaviour
+{
+	public GameObject foreshadowEvent;
+
+	void OnTriggerEnter(Collider other)
+	{
+		//if (string.Compare(other.transform.name, "O") == 0)
+		{
+			Debug.Log("Triggered!");
+			Instantiate(foreshadowEvent, transform.position, transform.rotation);
+		}
+	}
+}
