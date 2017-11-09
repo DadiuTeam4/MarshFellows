96b13746f53729cf86738a87f47ab90a1fb07733
Jonathan <madsof4@hotmail.com>
2017-11-09 15:50:47 +0100

    first draft of controlzone for hunter speed

---
+++ Assets/Scripts/Entities/HunterSpeedControlZone.cs
@@ -0,0 +1,30 @@
+﻿// Author: Jonathan
+// Contributers:
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterSpeedControlZone : MonoBehaviour
+{
+	public float speedZoneSpeed;
+	public float speedAfterZone = 1.6f;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (string.Compare(other.transform.name, "O") == 0)
+		{
+			var navMesh = other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+			navMesh.speed = speedZoneSpeed;			
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (string.Compare(other.transform.name, "O") == 0)
+		{
+			var navMesh = other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+			navMesh.speed = speedAfterZone;			
+		}
+	}
+}
