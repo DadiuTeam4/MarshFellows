ab7cb0ea547ac2a9e019665f6094109a9a33c421
Peter <pjager13@student.aau.dk>
2017-11-09 09:47:35 +0100

    deerprefab

---
+++ Assets/Shader/DeerAnimationController.cs
@@ -0,0 +1,63 @@
+﻿// Author: Peter Jæger
+// Contributors: 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerAnimationController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	Animator anim;
+	Rigidbody rigidbody;
+	float currentTime;
+	int reactHash = Animator.StringToHash("deerReact");
+	int RunHash = Animator.StringToHash("deerSpeed");
+
+	[SerializeField]
+	private float runDelay = 3.0f;
+
+	[SerializeField]
+	private Vector3 targetPoint;
+
+	[SerializeField]
+	private float turnRate = 10.0f;
+
+	[SerializeField]
+	private float runSpeed = 100.0f;
+
+	void Start () {
+		anim = GetComponentInChildren<Animator>();
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			anim.SetTrigger(reactHash);
+			currentTime = Time.time;
+		}
+		
+		if(currentTime + stateInfo.length + runDelay < Time.time)
+		{
+			Vector3 relativePos = targetPoint - transform.position;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			Vector3 v3Force = runSpeed * transform.forward;
+			rigidbody.rotation = Quaternion.RotateTowards(transform.rotation, rotation, turnRate);
+			rigidbody.AddForce(v3Force);
+			anim.SetFloat("deerSpeed", rigidbody.velocity.magnitude);
+		}
+		if((transform.position - targetPoint).magnitude < 2)
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+
+
+
+}
