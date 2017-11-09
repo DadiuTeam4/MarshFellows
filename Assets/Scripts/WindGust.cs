cb591898982761ec092c093bde0cb4924c188479
IYavin <itai.yavin@gmail.com>
2017-11-09 15:07:04 +0100

    Windgusts now stop when touch stops.

--- Assets/Scripts/WindGust.cs
+++ Assets/Scripts/WindGust.cs
@@ -9,21 +9,38 @@ using Events;
 [RequireComponent(typeof(WindZone))]
 public class WindGust : MonoBehaviour 
 {
+	[Tooltip("The amount of force applied from the wind zone component. Negative numbers push, while positive numbers pull")]
+	public float windForce = -1.0f;
+
 	private WindZone windZone;
 	private ParticleSystem windParticles;
 
-	private EventDelegate eventDelegate;
+	private EventDelegate onSwipe;
+	private EventDelegate onSwipeEnd;
 
 	void Start () 
 	{	
 		windZone = GetComponent<WindZone>();
 
-		eventDelegate = PlaceGust;
-		EventManager.GetInstance().AddListener(CustomEvent.Swipe, eventDelegate);
+		onSwipe = PlaceGust;
+		onSwipeEnd = DisableGust;
+		EventManager.GetInstance().AddListener(CustomEvent.Swipe, onSwipe);
+		EventManager.GetInstance().AddListener(CustomEvent.SwipeEnded, onSwipeEnd);
+	}
+
+	public void DisableGust(EventArgument argument)
+	{
+		Debug.Log("DISABLED");
+		windZone.windMain = 0;
 	}
 
 	public void PlaceGust(EventArgument argument)
 	{
+		if (windZone.windMain == 0)
+		{
+			windZone.windMain = windForce;
+		}
+
 		transform.position = argument.raycastComponent.point;
 	}
 }
