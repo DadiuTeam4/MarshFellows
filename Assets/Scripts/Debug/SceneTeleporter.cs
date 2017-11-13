599157e07959015f41d16e92b39a346d41fc79f1
Mathias Dam Hedelund <mdhe@itu.dk>
2017-11-13 15:08:27 +0100

    WIP on debug ui panel (scene teleporter is non-functioning)

---
+++ Assets/Scripts/Debug/SceneTeleporter.cs
@@ -0,0 +1,37 @@
+﻿// Author: Mathias Dam Hedelund
+// Contributors: 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Events;
+
+public class SceneTeleporter : MonoBehaviour 
+{
+	private Dropdown dropdown; 
+	private EventManager eventManager;
+
+	void Awake()
+	{
+		dropdown = GetComponent<Dropdown>();
+	}
+
+	void Start()
+	{
+		eventManager = EventManager.GetInstance();
+		for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+		{
+			dropdown.options.Add(new Dropdown.OptionData(SceneManager.GetSceneByBuildIndex(i).name));
+		}
+	}
+
+	public void LoadScene()
+	{
+		EventArgument argument = new EventArgument();
+		argument.stringComponent = dropdown.options[dropdown.value].text;
+		argument.intComponent = dropdown.value;
+		eventManager.CallEvent(CustomEvent.LoadScene, argument);
+		print(dropdown.options[dropdown.value].text + " loading.");
+	}
+}
