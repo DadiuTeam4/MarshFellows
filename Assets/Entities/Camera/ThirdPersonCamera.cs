e476aa37b312d18a021589628e532aae3d4c3eef
You Wu <3085031@sina.com>
2017-11-13 11:18:47 +0100

    Make the camera rotates as the player rotates and add a vector to adjust camera position.
    Make the camera rotates  as the camera rotates.

--- Assets/Entities/Camera/ThirdPersonCamera.cs
+++ Assets/Entities/Camera/ThirdPersonCamera.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 
 namespace CameraControl
 {
-    public class ThirdPersonCamera : BaseCamera 
+    public class ThirdPersonCamera : BaseCamera
     {
+        public Vector3 ajustablePos;
         public float positionDamping = 1;
         public float rotationDamping = 1;
-        
         private Vector3 offset;
-    
+
         private void Start()
         {
             controller = CameraStateController.GetInstance();
@@ -24,13 +24,13 @@ namespace CameraControl
             float currentAngle = transform.eulerAngles.y;
             float desiredAngle = controller.targets[0].eulerAngles.y;
             float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * rotationDamping);
-            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            Quaternion rotation = Quaternion.Euler(transform.eulerAngles.x, angle, 0);
+            transform.rotation = rotation;
 
-            Vector3 desiredPosition = deltaPosition + ((offset + deltaDistance));
+            Vector3 desiredPosition = controller.targets[0].position - (rotation * Vector3.forward * offset.magnitude) + ajustablePos;
             Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * positionDamping);
 
             transform.position = position;
-            transform.LookAt(deltaPosition);
         }
     }
 }
