200e89603ac3a095948f7c00f5bb80af2833963a
You Wu <3085031@sina.com>
2017-11-14 09:59:28 +0100

    Make the camera following P on separation event.
    Make the camera following P on separation event.

--- Assets/Entities/Camera/CinematicCamera.cs
+++ Assets/Entities/Camera/CinematicCamera.cs
@@ -3,52 +3,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
 namespace CameraControl
 {
-	// Scenario enum definition
-	public enum Scenario
-	{
-		Separation,
-		Ritual,
-		Deer,
-		Bear
-	}
+    // Scenario enum definition
+    public enum Scenario
+    {
+        Separation,
+        Ritual,
+        Deer,
+        Bear
+    }
 
-	public class CinematicCamera : BaseCamera 
-	{
-		public CinematicAnimation[] scenarioAnimations;
-		public float damping = 1;
+    public class CinematicCamera : BaseCamera
+    {
+        public CinematicAnimation[] scenarioAnimations;
+        public float damping = 1;
 
-		private Scenario currentScenario;
-		private CinematicAnimation currentAnimation;
-		private Vector3 scenarioStartPosition;
-		private Vector3 scenarioEndPosition;
+        private Scenario currentScenario;
+        private CinematicAnimation currentAnimation;
+        private Vector3 scenarioStartPosition;
+        private Vector3 scenarioEndPosition;
+        public bool isCameraFolloingP = false;
 
-		protected override void UpdatePosition()
-		{
-			float distanceFromBeginningToHunters = Vector3.Distance(deltaPosition, scenarioStartPosition);
-			float distanceFromEndToHunters = Vector3.Distance(deltaPosition, scenarioEndPosition);
-			float progress = distanceFromBeginningToHunters / (distanceFromBeginningToHunters + distanceFromEndToHunters);
+        void Awake()
+        {
+            // Add event listeners
+            EventDelegate eventDelegate = SeparationCallBack;
+            EventManager.GetInstance().AddListener(CustomEvent.ScenarioInteracted, SeparationCallBack);
+        }
 
-			Vector3 desiredPosition = currentAnimation.GetPosition(progress, deltaPosition);
-			Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
-			transform.position = position;
+        protected override void UpdatePosition()
+        {
+            float distanceFromBeginningToHunters = Vector3.Distance(deltaPosition, scenarioStartPosition);
+            float distanceFromEndToHunters = Vector3.Distance(deltaPosition, scenarioEndPosition);
+            float progress = distanceFromBeginningToHunters / (distanceFromBeginningToHunters + distanceFromEndToHunters);
 
-			Vector3 lookAtPos = new Vector3();
-			lookAtPos.x = currentAnimation.focusOnHuntersX ? deltaPosition.x : currentAnimation.center.x;
-			lookAtPos.y = currentAnimation.focusOnHuntersY ? deltaPosition.y : currentAnimation.center.y;
-			lookAtPos.z = currentAnimation.focusOnHuntersZ ? deltaPosition.z : currentAnimation.center.z;
-			transform.LookAt(lookAtPos);
-		}
+            Vector3 desiredPosition = currentAnimation.GetPosition(progress, deltaPosition);
+            Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+            transform.position = position;
 
-		public void SetScenario(Scenario scenario, Vector3 scenarioStartPosition, Vector3 scenarioEndPosition)
-		{
-			currentScenario = scenario;
-			currentAnimation = scenarioAnimations[(int) scenario];
+            Vector3 lookAtPos = new Vector3();
+            if (isCameraFolloingP)
+            {
+                lookAtPos = controller.targets[0].position;
+            }
+            else
+            {
+                lookAtPos.x = currentAnimation.focusOnHuntersX ? deltaPosition.x : currentAnimation.center.x;
+                lookAtPos.y = currentAnimation.focusOnHuntersY ? deltaPosition.y : currentAnimation.center.y;
+            }
 
-			this.scenarioStartPosition = scenarioStartPosition;
-			this.scenarioEndPosition = scenarioEndPosition;
-		}
-	}
+            transform.LookAt(lookAtPos);
+        }
+
+        public void SetScenario(Scenario scenario, Vector3 scenarioStartPosition, Vector3 scenarioEndPosition)
+        {
+            currentScenario = scenario;
+            currentAnimation = scenarioAnimations[(int)scenario];
+
+            this.scenarioStartPosition = scenarioStartPosition;
+            this.scenarioEndPosition = scenarioEndPosition;
+        }
+
+        private void SeparationCallBack(EventArgument argument)
+        {
+            isCameraFolloingP = true;
+        }
+
+    }
 }
