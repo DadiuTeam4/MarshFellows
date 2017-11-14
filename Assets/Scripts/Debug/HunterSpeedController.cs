8a4e983623ff9cadaea7e8009b65cc82d00aa2a3
IYavin <itai.yavin@gmail.com>
2017-11-14 14:48:14 +0100

    Debug UI now allows for altering hunter speed.

--- Assets/Scripts/Debug/HunterSpeedController.cs
+++ Assets/Scripts/Debug/HunterSpeedController.cs
@@ -8,11 +8,21 @@ using UnityEngine.AI;
 public class HunterSpeedController : MonoBehaviour 
 {
 	[SerializeField]
-	private NavMeshAgent[] hunters;
+	private List<NavMeshAgent> hunters = new List<NavMeshAgent>();
 
 	void Start()
 	{
-
+		GameObject[] hunterGameObjects = GameObject.FindGameObjectsWithTag("Player");
+		
+		for (int i = 0; i < hunterGameObjects.Length; i++)
+		{
+			NavMeshAgent agent = hunterGameObjects[i].GetComponent<NavMeshAgent>();
+			
+			if (agent)
+			{
+				hunters.Add(agent);
+			}
+		}
 	}
 
 	public void SetSpeed(float speed) 
