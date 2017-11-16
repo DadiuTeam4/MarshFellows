cc495d0f79250f0d55745a0cd08fe86d35691fd4
You Wu <3085031@sina.com>
2017-11-16 16:29:12 +0100

    Added functionality for tree fall down.

---
+++ Assets/Scripts/ObjectFall.cs
@@ -0,0 +1,42 @@
+﻿// Author: You Wu
+// Contributors: 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFall : MonoBehaviour
+{
+    private Quaternion initialRotation;
+    private bool hasFall;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+        hasFall = false;
+    }
+
+    void Update()
+    {
+        if (!hasFall)
+        {
+            CheckIfFall();
+        }
+
+    }
+
+    private void CheckIfFall()
+    {
+        Quaternion currentRotation = transform.rotation;
+        if (Mathf.Abs(currentRotation.eulerAngles.x - initialRotation.eulerAngles.x) > 180 )
+        {
+			Debug.Log( initialRotation.eulerAngles.x);
+			Debug.Log( currentRotation.eulerAngles.x);
+			
+			Debug.Log(Mathf.Abs(currentRotation.eulerAngles.x - initialRotation.eulerAngles.x));
+            Debug.Log(gameObject.name + " Fall");
+			//Debug.Log(Mathf.Abs(currentRotation.eulerAngles.z - initialRotation.eulerAngles.z));
+			
+            hasFall = true;
+        }
+    }
+}
