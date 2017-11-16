868915d47aa13be34eba8cb2aa6e309e2a3b51f9
You Wu <3085031@sina.com>
2017-11-16 11:06:49 +0100

    Make sure scences are valid when try to unload and load scene from build index.
    .
    Make sure scences are valid when try to unload and load scene from build index.

--- Assets/Scripts/Systems/SceneLoaderManager.cs
+++ Assets/Scripts/Systems/SceneLoaderManager.cs
@@ -135,7 +135,7 @@ public class SceneLoaderManager : Singleton<SceneLoaderManager>
 				continue;
 			}
             Scene scene = SceneManager.GetSceneByBuildIndex(i);
-            if (!scene.isLoaded || scene.name != globalSceneName)
+            if ((!scene.isLoaded || scene.name != globalSceneName)&& scene.IsValid())
             {
                 if (Application.isPlaying)
                 {
