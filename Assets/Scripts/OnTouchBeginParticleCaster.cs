fde77ff25ebc61656072cdbbc44da52daf184563
IYavin <itai.yavin@gmail.com>
2017-11-17 15:03:55 +0100

    wtf...

---
+++ Assets/Scripts/OnTouchBeginParticleCaster.cs
@@ -0,0 +1,23 @@
+﻿// Author: Itai Yavin
+// Contributors: 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+public class OnTouchBeginParticleCaster : MonoBehaviour 
+{
+	private EventDelegate placeParticle;
+	
+	void Start () 
+	{
+		placeParticle = PlaceParticleSystem;
+		EventManager.GetInstance().AddListener(CustomEvent.TouchBegan, placeParticle);
+	}
+	
+	private void PlaceParticleSystem(EventArgument argument)
+	{
+
+	}
+}
