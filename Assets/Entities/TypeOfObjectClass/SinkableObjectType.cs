d5fc40aa9fcfeb41bcbd14135bd103161f130919
You Wu <3085031@sina.com>
2017-11-17 11:06:04 +0100

    Added script to check if the obejct is sunk.

--- Assets/Entities/TypeOfObjectClass/SinkableObjectType.cs
+++ Assets/Entities/TypeOfObjectClass/SinkableObjectType.cs
@@ -1,10 +1,70 @@
 ﻿//Author:Tilemachos
-//Co-author:
+//Co-author: You Wu
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SinkableObjectType : MonoBehaviour {
+public class SinkableObjectType : MonoBehaviour
+{
 
-	public string typeOfSinkable = "tree";
+    private Vector3 initialPosition;
+    public string typeOfSinkable = "tree";
+    private bool hasSunk;
+    public Transform groundTransform;
+    void Start()
+    {
+        initialPosition = transform.position;
+        InitStatusOfSink();
+    }
+
+    private void InitStatusOfSink()
+    {
+        if (groundTransform == null)
+        {
+            enabled = false;
+        }
+        else
+        {
+            if (transform.position.y < groundTransform.position.y)
+            {
+                //Here are objects that are sunk at start
+				Debug.Log(gameObject.name + " has sunk at start!");
+                hasSunk = true;
+                enabled = false;
+            }
+            else
+            {
+                hasSunk = false;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        if (!hasSunk && groundTransform != null)
+        {
+            CheckIfSunk();
+        }
+
+    }
+    private void CheckIfSunk()
+    {
+        if (IsSunk())
+        {
+            CallSunkEvent();
+        }
+    }
+
+    private bool IsSunk()
+    {
+        return transform.position.y < initialPosition.y && transform.position.y < groundTransform.position.y;
+    }
+
+    private void CallSunkEvent()
+    {
+        hasSunk = true;
+        enabled = false;
+        Debug.Log(typeOfSinkable + " has sunk " + gameObject.name);
+    }
 }
