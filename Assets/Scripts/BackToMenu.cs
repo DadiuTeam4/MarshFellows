4175dce247a1ca7b4c4a83266886e402364e9db8
You Wu <3085031@sina.com>
2017-11-17 15:07:00 +0100

    Added sound effect for clicking back button.

--- Assets/Scripts/BackToMenu.cs
+++ Assets/Scripts/BackToMenu.cs
@@ -7,9 +7,16 @@ using UnityEngine;
 public class BackToMenu : MonoBehaviour
 {
 	public Transform menuPanel;
+    public AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = AudioManager.GetInstance();
+    }
     public void Back()
     {
 		menuPanel.gameObject.SetActive(true);
         transform.parent.gameObject.SetActive(false);
+        audioManager.OnMenuClick();
     }
 }
