dad1ca78261e9521603ee1743e8c5eb3b0f59d88
Emil <emimvi@gmail.com>
2017-11-24 13:47:11 +0100

    Deer look at O when reacting

--- Assets/Scripts/Entities/IkHandlerGeneric.cs
+++ Assets/Scripts/Entities/IkHandlerGeneric.cs
@@ -12,30 +12,29 @@ public class IkHandlerGeneric : MonoBehaviour
 
     public Vector3 Offset;
 
-    public Transform lookPos;
+    private Transform lookPos;
+
+    public string tagToLookAt = "O";
 
     public Transform head;
     public Transform neckA;
-    public Transform neckB;
     public Transform chest;
 
     [Range(0, 1)] public float headWeight = 1;
-    [Range(0, 1)] public float neckWeight = 1;
-    [Range(0, 1)] public float chestWeight = 1;
+    [Range(0, 1)] public float neckWeight = 0.66f;
+    [Range(0, 1)] public float chestWeight = 0.33f;
 
+    private bool lookAtTarget = false;
 
-    Vector3 currentPosDir;
-
-    // Use this for initialization
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        GameObject target = GameObject.FindGameObjectWithTag(tagToLookAt);
+        if (target)
+        {
+            lookPos = target.transform;
+        }
     }
 
     Vector3 CalulateWeightedLook(Transform bone, float weight)
@@ -48,23 +47,29 @@ public class IkHandlerGeneric : MonoBehaviour
 
     private void LateUpdate()
     {
-        //Transform bone = head;
-        //float distance = Vector3.Distance(bone.position, lookPos.position);
-        //Vector3 forwardPoint = bone.position + Quaternion.Euler(new Vector3(0, -90, 0)) * transform.forward * distance;
-        //Vector3 result = forwardPoint + (lookPos.position - forwardPoint) * headWeight;
-
-        chest.LookAt(CalulateWeightedLook(chest, chestWeight));
-        chest.rotation = chest.rotation * Quaternion.Euler(Offset);
-
-        neckA.LookAt(CalulateWeightedLook(neckA, neckWeight));
-        neckA.rotation = neckA.rotation * Quaternion.Euler(Offset);
-
-        head.LookAt(CalulateWeightedLook(head, headWeight));
-        head.rotation = head.rotation * Quaternion.Euler(Offset);
+        if (lookAtTarget && lookPos)
+        {
+            chest.LookAt(CalulateWeightedLook(chest, chestWeight));
+            chest.rotation *= Quaternion.Euler(0, 90, 0);
+            neckA.LookAt(CalulateWeightedLook(neckA, neckWeight));
+            neckA.rotation *= Quaternion.Euler(0, 90, 0);
+            head.LookAt(CalulateWeightedLook(head, headWeight));
+            head.rotation *= Quaternion.Euler(0, 90, 0);
+        }
 
     }
 
-    private void OnDrawGizmos()
+    public void LookForward()
+    {
+        lookAtTarget = false;
+    }
+
+    public void LookAtHunters()
+    {
+        lookAtTarget = true;
+    }
+
+    /* private void OnDrawGizmos()
     {
         Vector3 currentPosDir = Quaternion.Euler(new Vector3(0, -90, 0)) * head.forward;
 
@@ -78,6 +83,6 @@ public class IkHandlerGeneric : MonoBehaviour
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(forwardPoint, forwardPoint + (lookPos.position - forwardPoint) * headWeight);
         
-    }
+    } */
 
 }
