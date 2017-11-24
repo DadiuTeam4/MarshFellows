d2876e170f083f8281f1c4ef50582d1f492d48d1
You Wu <rmq350@ku.dk>
2017-11-24 13:53:55 +0100

    Do not unload last scene because it is not supported.

--- Assets/Scripts/Systems/SceneLoaderManager.cs
+++ Assets/Scripts/Systems/SceneLoaderManager.cs
@@ -173,7 +173,8 @@ public class SceneLoaderManager : Singleton<SceneLoaderManager>
     void UnloadAllScenes(string unloadGlobal) 
     {
         int c = SceneManager.sceneCount;
-        for (int i = 0; i < c; i++) 
+        //Unload last scene is not supported
+        for (int i = 0; i < c - 1; i++) 
         {
             Scene scene = SceneManager.GetSceneAt (i);  
             if(scene.name != unloadGlobal)
