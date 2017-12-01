7926d9043d017df8ef44c0867d73a66861bf9b76
Unknown <mdhe@itu.dk>
2017-12-01 09:02:32 +0100

    Buildtweaks about fog and shizzle

--- Assets/Scripts/Ritual/Shaman.cs
+++ Assets/Scripts/Ritual/Shaman.cs
@@ -60,6 +60,6 @@ public class Shaman : MonoBehaviour {
     void Disrupted(EventArgument args)
     {
         disrupted = true;
-        GetComponentInChildren<MeshRenderer>().material.color = Color.red;
+        //GetComponentInChildren<MeshRenderer>().material.color = Color.red;
     }
 }
